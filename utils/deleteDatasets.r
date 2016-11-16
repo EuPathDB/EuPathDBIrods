0ed7406eb306200eb0257f9d2f0bb2a155b47a0b
Cris Lawrence <crisl@upenn.edu>
2016-11-16 15:57:05 +0000

    Improved query per Mark's suggestion
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/EuPathDBIrods/trunk@78360 735e2a04-f8fc-0310-8a1b-f2942603c481

--- utils/deleteDatasets.r
+++ utils/deleteDatasets.r
@@ -1,12 +1,10 @@
 utilDeleteAllDatasets {
          writeLine("stdout", "Starting deletion of all user datasets");
-		 *Datasets = SELECT COLL_NAME WHERE COLL_NAME like *Location;
+		 *Datasets = SELECT COLL_NAME WHERE COLL_PARENT_NAME like *Location;
          foreach(*Datasets) {
-		   if(*Datasets.COLL_NAME not like "/ebrc/workspaces/users/*/datasets/*/*") {
-             msiRmColl(*Datasets.COLL_NAME, *Flag, *Status);
-             writeLine("stdout", "Removed" ++ *Datasets.COLL_NAME);
-		   }	 
+           msiRmColl(*Datasets.COLL_NAME, *Flag, *Status);
+           writeLine("stdout", "Removed" ++ *Datasets.COLL_NAME); 
          }
  }
-input  *Location = "/ebrc/workspaces/users/%/datasets/%", *Flag="forceFlag="
+input  *Location = "/ebrc/workspaces/users/%/datasets", *Flag="forceFlag="
 output ruleExecOut
