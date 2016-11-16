8fce695be2ea03288f331dac247a4318af7dd481
Cris Lawrence <crisl@upenn.edu>
2016-11-16 18:36:27 +0000

    Utility rules for workspace cleanup during experimentation
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/EuPathDBIrods/trunk@78367 735e2a04-f8fc-0310-8a1b-f2942603c481

--- utils/deleteDatasets.r
+++ utils/deleteDatasets.r
@@ -3,7 +3,7 @@ utilDeleteAllDatasets {
 		 *Datasets = SELECT COLL_NAME WHERE COLL_PARENT_NAME like *Location;
          foreach(*Datasets) {
            msiRmColl(*Datasets.COLL_NAME, *Flag, *Status);
-           writeLine("stdout", "Removed" ++ *Datasets.COLL_NAME); 
+           writeLine("stdout", "Removed" ++ *Datasets.COLL_NAME);
          }
  }
 input  *Location = "/ebrc/workspaces/users/%/datasets", *Flag="forceFlag="
---
+++ utils/emptyEventsCollection.r
@@ -0,0 +1,12 @@
+utilEmptyEventsCollection {
+  writeLine("stdout", "Starting deletion of all events");
+  *results = SELECT DATA_NAME WHERE COLL_NAME == *eventCollection;
+  foreach(*results) {
+    *eventDataObject = *results.DATA_NAME;
+    *eventDataObjectPath = "*eventCollection/*eventDataObject";
+    msiDataObjUnlink("objPath=*eventDataObjectPath",*Status);
+	writeLine("stdout", "Removed *eventDataObject");
+  }
+}
+input  *eventCollection = "/ebrc/workspaces/events"
+output ruleExecOut
