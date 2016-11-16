0ddcd27fec038e22de5342442829b5b66f84a26a
Cris Lawrence <crisl@upenn.edu>
2016-11-16 20:11:07 +0000

    Corrected syntax error
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/EuPathDBIrods/trunk@78375 735e2a04-f8fc-0310-8a1b-f2942603c481

--- utils/datasetParser.py
+++ utils/datasetParser.py
@@ -6,6 +6,11 @@ import json
 # Utility to parse the dataset.json file accompanying a dataset and to
 # return the content in the form of a string that can be parsed by the
 # calling rule.
+
+
+# '{ "type": {"name": "RNA Seq", "version": "1.0"},"dependencies": [{"resourceIdentifier": "pf3d7_genome_rsrc",  "resourceVersion": "12/2/2015", "resourceDisplayName": "pfal genome"},  {"resourceIdentifier": "hsap_genome_rsrc",    "resourceVersion": "2.6",   "resourceDisplayName": "human genome"} ],"owner": 12345,"size": "200M","modified": 1248312231083,"created": 1231238088881,"uploaded": 1231398508344}'
+
+
 def main():
   args = sys.argv[1:]
   if len(args) == 0:
@@ -14,7 +19,7 @@ def main():
   parsedJson = json.loads(args[0])
   type = parsedJson['type']
   dependencies = parsedJson['dependencies']
-  output = "owner_user_id=" + type['owner'] + "%" + "ud_type_name=" + type['name'] + "%" + "ud_type_version=" + type['version'] + "%" + "dependency=" + dependencies[0]['resourceIdentifier'] + "%" + "dependency_version=" + dependencies[0]['resourceVersion']
+  output = "owner_user_id=" + str(parsedJson['owner']) + "%" + "ud_type_name=" + type['name'] + "%" + "ud_type_version=" + type['version'] + "%" + "dependency=" + dependencies[0]['resourceIdentifier'] + "%" + "dependency_version=" + dependencies[0]['resourceVersion']
   sys.stdout.write(output)
 
 if __name__ == "__main__":
