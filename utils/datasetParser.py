88262088aced89569a53bded00e0a98a36afc01d
Cris Lawrence <crisl@upenn.edu>
2016-11-17 16:38:51 +0000

    Added date time timestamp for possible use in events
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/EuPathDBIrods/trunk@78389 735e2a04-f8fc-0310-8a1b-f2942603c481

--- utils/datasetParser.py
+++ utils/datasetParser.py
@@ -2,6 +2,7 @@
 
 import sys
 import json
+from datetime import datetime
 
 # Utility to parse the dataset.json file accompanying a dataset and to
 # return the content in the form of a string that can be parsed by the
@@ -15,11 +16,11 @@ def main():
   args = sys.argv[1:]
   if len(args) == 0:
     raise IOError("A well-formed json string is required.")
-  rawJson = ''.join(args).replace('\n','')
+#  rawJson = ''.join(args).replace('\n','')
   parsedJson = json.loads(args[0])
   type = parsedJson['type']
   dependencies = parsedJson['dependencies']
-  output = "owner_user_id=" + str(parsedJson['owner']) + "%" + "ud_type_name=" + type['name'] + "%" + "ud_type_version=" + type['version'] + "%" + "dependency=" + dependencies[0]['resourceIdentifier'] + "%" + "dependency_version=" + dependencies[0]['resourceVersion']
+  output = "timestamp=" + str(datetime.now()) + "%" + "owner_user_id=" + str(parsedJson['owner']) + "%" + "ud_type_name=" + type['name'] + "%" + "ud_type_version=" + type['version'] + "%" + "dependency=" + dependencies[0]['resourceIdentifier'] + "%" + "dependency_version=" + dependencies[0]['resourceVersion']
   sys.stdout.write(output)
 
 if __name__ == "__main__":
