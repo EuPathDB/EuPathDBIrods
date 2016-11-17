0b2e44f98099606310d3921c6dc21cbb5d3e7363
Cris Lawrence <crisl@upenn.edu>
2016-11-17 16:57:31 +0000

    Added date time timestamp for possible use in events - corrected to be long format int
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/EuPathDBIrods/trunk@78392 735e2a04-f8fc-0310-8a1b-f2942603c481

--- utils/datasetParser.py
+++ utils/datasetParser.py
@@ -2,7 +2,7 @@
 
 import sys
 import json
-from datetime import datetime
+import time
 
 # Utility to parse the dataset.json file accompanying a dataset and to
 # return the content in the form of a string that can be parsed by the
@@ -20,7 +20,7 @@ def main():
   parsedJson = json.loads(args[0])
   type = parsedJson['type']
   dependencies = parsedJson['dependencies']
-  output = "timestamp=" + str(datetime.now()) + "%" + "owner_user_id=" + str(parsedJson['owner']) + "%" + "ud_type_name=" + type['name'] + "%" + "ud_type_version=" + type['version'] + "%" + "dependency=" + dependencies[0]['resourceIdentifier'] + "%" + "dependency_version=" + dependencies[0]['resourceVersion']
+  output = "timestamp=" + str(int(time.time() * 1000)) + "%" + "owner_user_id=" + str(parsedJson['owner']) + "%" + "ud_type_name=" + type['name'] + "%" + "ud_type_version=" + type['version'] + "%" + "dependency=" + dependencies[0]['resourceIdentifier'] + "%" + "dependency_version=" + dependencies[0]['resourceVersion']
   sys.stdout.write(output)
 
 if __name__ == "__main__":
