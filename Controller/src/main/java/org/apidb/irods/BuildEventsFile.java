9852acda381852ab464434ecbd3fcf5bb4a4dbd4
Cris Lawrence <crisl@upenn.edu>
2016-12-05 20:25:40 +0000

    Rough first pass at producing an events file from the event files in the event folder.
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/EuPathDBIrods/trunk@78627 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Controller/src/main/java/org/apidb/irods/BuildEventsFile.java
+++ Controller/src/main/java/org/apidb/irods/BuildEventsFile.java
@@ -1,15 +1,57 @@
 package org.apidb.irods;
 
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.gusdb.wdk.model.Utilities;
+import org.gusdb.wdk.model.WdkModelException;
+import org.gusdb.wdk.model.config.ModelConfig;
+import org.gusdb.wdk.model.config.ModelConfigParser;
+import org.gusdb.wdk.model.config.ModelConfigUserDatasetStore;
+import org.gusdb.wdk.model.user.dataset.UserDatasetStore;
+import org.gusdb.wdk.model.user.dataset.filesys.FilesysUserDatasetStore;
+import org.gusdb.wdk.model.user.dataset.irods.IrodsUserDatasetStoreAdaptor;
+import org.gusdb.wdk.model.user.dataset.json.JsonUserDatasetStoreAdaptor;
 import org.irods.jargon.core.pub.io.IRODSFile;
+import org.xml.sax.SAXException;
 
 public class BuildEventsFile {
+  public static final String EVENTS_DIR = "/ebrc/workspaces/events";
 	
-  public static void main(String[] args) {
+  public static void main(String[] args) throws Exception {
 	String projectId = System.getenv("PROJECT_ID");
-	String gusHome = System.getenv("GUS_HOME");
-	String configPath = gusHome + "/config/" + projectId + "model-config.xml";
-	 
-	 
+	String gusHome = System.getProperty(Utilities.SYSTEM_PROPERTY_GUS_HOME);     
+    ModelConfigParser parser = new ModelConfigParser(gusHome);
+    ModelConfig modelConfig = null;
+	try {
+	  modelConfig = parser.parseConfig(projectId);
+	} 
+	catch (WdkModelException | SAXException | IOException e) {
+	  e.printStackTrace();
+	  throw new RuntimeException(e);
+	}
+	ModelConfigUserDatasetStore udsConfig = modelConfig.getUserDatasetStoreConfig();
+    UserDatasetStore uds = udsConfig.getUserDatasetStore();
+    //TODO use userdatasetstore.getadaptor when available.
+	JsonUserDatasetStoreAdaptor udsa = new IrodsUserDatasetStoreAdaptor();
+	List<Path> eventFiles = new ArrayList<>();
+	try {
+	  eventFiles = udsa.getPathsInDir(Paths.get(EVENTS_DIR));
+	}
+	catch (WdkModelException e) {
+	  e.printStackTrace();
+	  throw new RuntimeException(e);
+	}
+	StringBuilder events = new StringBuilder();
+	for(Path eventFile : eventFiles) {
+	  String event = udsa.readFileContents(eventFile);
+	  events.append(event + System.getProperty("line.separator"));
+	}
+	System.out.println(events.toString());
+  
   }
 
 }
