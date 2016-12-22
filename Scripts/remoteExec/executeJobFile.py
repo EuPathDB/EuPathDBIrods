a9dd48764b2b70824eb85fe3341c200531c70a6c
Cris Lawrence <crisl@upenn.edu>
2016-12-22 21:23:01 +0000

    Now using shared workspace
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/EuPathDBIrods/trunk@79049 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Scripts/remoteExec/executeJobFile.py
+++ Scripts/remoteExec/executeJobFile.py
@@ -11,9 +11,8 @@ from urlparse import urlparse
 # Done with the generous assistance of Mark Heiges
 
 # Example parameter list
-# "wrkspuser,be4797e4b88200492d29cf0aeb32f5de,http://wij.vm:9171/job/IrodsListener/buildWithParameters,eupathdbirods,otherstuff
-# PlasmoDB
-# CryptoDB"
+# "wrkspuser,be4797e4b88200492d29cf0aeb32f5de,http://wij.vm:9171/job/IrodsListener/build,eupathdbirods,otherstuff"
+
 
 # This program resides in /var/lib/irods/iRODS/server/bin/cmd and is set as owned by irods.
 
@@ -21,26 +20,21 @@ def main():
   args = sys.argv[1:]
   if len(args) != 1:
     raise IOError("Expected a single string argument containing job file path, username, and password separated with commas.")
-  params = "".join(args).split("\n")
-  props = params[0].split(",")
+  props = "".join(args).split(",")
   username = props[0]
   password = props[1]
   jobUrl = props[2]
   token = props[3]
   datasetStoreId = props[4]
   host = urlparse(jobUrl).scheme + "://" + urlparse(jobUrl).hostname + ":" + str(urlparse(jobUrl).port)
-  jobParams = params[1:]
   crumbUrl = host + "/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,\":\",//crumb)"
   response = requests.get(crumbUrl, auth=(username, password))
   sys.stdout.write("Crumb retrieval Status Code: " + str(response.status_code) + "\n")
   crumbValue = response.text.split(":")[1]
   headers = {"Jenkins-Crumb":crumbValue}
-  for jobParam in jobParams:
-    if len(jobParam) > 0:
-      sys.stdout.write("Job Param: " + jobParam)
-      params = {"PROJECT_ID": jobParam, "DATASET_STORE_ID": datasetStoreId, "token": token}
-      response = requests.post(jobUrl, auth=(username, password), headers=headers, data=params)
-      sys.stdout.write(" - Status Code: " + str(response.status_code) + "\n")
+  params = {"DATASET_STORE_ID": datasetStoreId, "token": token}
+  response = requests.post(jobUrl, auth=(username, password), headers=headers, data=params)
+  sys.stdout.write(" - Status Code: " + str(response.status_code) + "\n")
   sys.stdout.write("Complete\n")
   
 if __name__ == "__main__":
