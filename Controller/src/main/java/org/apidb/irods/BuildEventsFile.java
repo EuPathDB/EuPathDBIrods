335360b35e14c8c7b73ead2b6c615cd0d12058bd
Ryan Doherty <tech@conical.org>
2017-01-11 04:26:16 +0000

    fix whitespace
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/EuPathDBIrods/trunk@79174 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Controller/src/main/java/org/apidb/irods/BuildEventsFile.java
+++ Controller/src/main/java/org/apidb/irods/BuildEventsFile.java
@@ -18,7 +18,6 @@ import org.gusdb.wdk.model.user.dataset.UserDatasetStoreAdaptor;
 import org.gusdb.wdk.model.user.dataset.event.UserDatasetEventListHandler;
 import org.xml.sax.SAXException;
 
-
 /**
  * This is a small Java application that should be called by Jenkins whenever an IRODS event of note takes
  * place and also periodically, given the possibility that a prior event was not captured.  It reads the
@@ -26,60 +25,61 @@ import org.xml.sax.SAXException;
  * that is passed on to the WDK's user dataset events handler to processing according to the nature of each
  * event. Although this file addresses event handling is intended for IRODS, it has no specific references
  * to IRODS.  It could be used with a POSIX system as well.
+ * 
  * @author crisl-adm
- *
  */
 public class BuildEventsFile {
 
   private static final Logger logger = Logger.getLogger(BuildEventsFile.class);	
   public static final String EVENTS_DIR = "/ebrc/workspaces/events";
-	
+
   public static void main(String[] args) throws Exception {
-	String projectId = System.getenv("PROJECT_ID");
-	String datasetStoreId = System.getenv("DATASET_STORE_ID");
-	logger.info("Parameters - Project: " + projectId + ", Dataset Store: " + datasetStoreId);
-	String gusHome = System.getProperty(Utilities.SYSTEM_PROPERTY_GUS_HOME);     
+    String projectId = System.getenv("PROJECT_ID");
+    String datasetStoreId = System.getenv("DATASET_STORE_ID");
+    logger.info("Parameters - Project: " + projectId + ", Dataset Store: " + datasetStoreId);
+    String gusHome = System.getProperty(Utilities.SYSTEM_PROPERTY_GUS_HOME);     
     ModelConfigParser parser = new ModelConfigParser(gusHome);
     ModelConfig modelConfig = null;
-	try {
-	  modelConfig = parser.parseConfig(projectId);
-	} 
-	catch(WdkModelException | SAXException | IOException e) {
-	  e.printStackTrace();
-	  throw new RuntimeException(e);
-	}
-	ModelConfigUserDatasetStore udsConfig = modelConfig.getUserDatasetStoreConfig();
+    try {
+      modelConfig = parser.parseConfig(projectId);
+    }
+    catch(WdkModelException | SAXException | IOException e) {
+      e.printStackTrace();
+      throw new RuntimeException(e);
+    }
+    ModelConfigUserDatasetStore udsConfig = modelConfig.getUserDatasetStoreConfig();
     UserDatasetStore uds = udsConfig.getUserDatasetStore();
     // Insures that only the correct IRODS is calling this method.
     if(!uds.getUserDatasetStoreId().equals(datasetStoreId)) {
       throw new RuntimeException("Called by wrong IRODS " + datasetStoreId);
     }
-	UserDatasetStoreAdaptor udsa = uds.getUserDatasetStoreAdaptor();
-	List<Path> eventFiles = new ArrayList<>();
-	try {
-	  eventFiles = udsa.getPathsInDir(Paths.get(EVENTS_DIR));
-	}
-	catch (WdkModelException e) {
-	  e.printStackTrace();
-	  throw new RuntimeException(e);
-	}
-	StringBuilder events = new StringBuilder();
-	List<String> eventList = new ArrayList<>();
-	for(Path eventFile : eventFiles) {
-	  String eventFilename = eventFile.getFileName().toString();
-	  // The timestamp used to make each event file unique is extracted from the event filename and
-	  // pre-pended to the event content to serve as an event id.
-	  String timestamp = eventFilename.substring(eventFilename.indexOf('_') + 1, eventFilename.indexOf('.'));
-	  String event = udsa.readFileContents(eventFile);
-	  events.append(timestamp + "\t" + event + System.getProperty("line.separator"));
-	  eventList.add(timestamp + "\t" + event);
-	} 
-	System.out.println(events.toString());
-	String cmdName = System.getProperty("cmdName");
-	UserDatasetEventListHandler handler = new UserDatasetEventListHandler(cmdName);
-	handler.setProjectId(projectId);
-	Path tmpDir =  Paths.get(handler.getWdkTempDirName());
-    handler.handleEventList(handler.parseEventsList(eventList), modelConfig.getUserDatasetStoreConfig().getTypeHandlers(), tmpDir);
+    UserDatasetStoreAdaptor udsa = uds.getUserDatasetStoreAdaptor();
+    List<Path> eventFiles = new ArrayList<>();
+    try {
+      eventFiles = udsa.getPathsInDir(Paths.get(EVENTS_DIR));
+    }
+    catch (WdkModelException e) {
+      e.printStackTrace();
+      throw new RuntimeException(e);
+    }
+    StringBuilder events = new StringBuilder();
+    List<String> eventList = new ArrayList<>();
+    for(Path eventFile : eventFiles) {
+      String eventFilename = eventFile.getFileName().toString();
+      // The timestamp used to make each event file unique is extracted from the event filename and
+      // pre-pended to the event content to serve as an event id.
+      String timestamp = eventFilename.substring(eventFilename.indexOf('_') + 1, eventFilename.indexOf('.'));
+      String event = udsa.readFileContents(eventFile);
+      events.append(timestamp + "\t" + event + System.getProperty("line.separator"));
+      eventList.add(timestamp + "\t" + event);
+    }
+    System.out.println(events.toString());
+    String cmdName = System.getProperty("cmdName");
+    UserDatasetEventListHandler handler = new UserDatasetEventListHandler(cmdName);
+    handler.setProjectId(projectId);
+    Path tmpDir =  Paths.get(handler.getWdkTempDirName());
+    handler.handleEventList(UserDatasetEventListHandler.parseEventsList(eventList),
+        modelConfig.getUserDatasetStoreConfig().getTypeHandlers(), tmpDir);
   }
 
 }
