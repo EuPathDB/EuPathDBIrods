8d3615a36d13e8334cfcb0f19ce58def5a389399
Cris Lawrence <crisl@upenn.edu>
2017-01-17 18:12:47 +0000

    Refactored since original was more basic POC.  This is a bit better to follow.
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/EuPathDBIrods/trunk@79251 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Scripts/galaxyTools/exportGeneListToEuPathDB.py
+++ Scripts/galaxyTools/exportGeneListToEuPathDB.py
@@ -8,13 +8,15 @@ import os
 import shutil
 import tarfile
 import requests
+import sys
 from requests.auth import HTTPBasicAuth
+import contextlib
 
 '''
  The following program is a Galaxy Tool for exporting gene list data from Galaxy to EuPathDB via IRODS.
 
  Sample for testing outside of Galaxy:
- python exportGeneListToEuPathDB.py "a name" "a description" "a summary" "test-data/genelist.txt" "test-data/output.txt" "108976930"
+ python exportGeneListToEuPathDB.py "a name" "a description" "a summary" "PlasmoDB" "test-data/genelist.txt" "test-data/output.txt" "108976930"
 '''
 
 def __main__():
@@ -22,35 +24,65 @@ def __main__():
     parser = optparse.OptionParser()
     (options, args) = parser.parse_args()
 
-    timestamp = int(time.time() * 1000)
 
-    datasetName = args[0]
+    # Salt away all parameters.
+    dataset_name = args[0]
     description = args[1]
     summary = args[2]
-    datasetFile = args[3]
-    results = args[4]
-    userId = args[5]
+    project = args[3]
+    dataset_file = args[4]
+    results = args[5]
+    user_id = args[6]
     # My WDK user id for now
-    userId = "108976930"
+    user_id = "108976930"
+
+    # This msec timestamp is used to denote both the created and modified times.
+    timestamp = int(time.time() * 1000)
+
+    # TODO host, port, username and password will need to go into a configuration file somehow
+    REST_URL = "http://wij.vm:8180/irods-rest/rest/fileContents/ebrc/workspaces/lz/"
+    USER = "wrkspuser"
+    PWD = "passWORD"
+
+    # Names for the 2 json files and the folder containing the dataset to be included in the tarball
+    META_JSON = "meta.json"
+    DATASET_JSON = "dataset.json"
+    DATAFILES = "datafiles"
+
+    # Name given to this type of dataset and to the expected file
+    GENE_LIST_TYPE = "GeneList"
+    GENE_LIST_FILE = "genelist.txt"
+
+    # This is the name of the file to be exported sans extension.  It will be used to designate a unique temporary
+    # directory and to export both the tarball and the flag that triggers IRODS to process the tarball. By
+    # convention, the dataset tarball is of the form dataset_uNNNNNN_tNNNNNNN.tgz where the NNNNNN following the _u
+    # is the WDK user id and _t is the msec timestamp
+    export_file_root = 'dataset_u' + user_id + '_t' + str(timestamp)
 
     # Create a unique temporary directory that serve as a staging area for assembling the tarball.
-    tempPath = tempfile.mkdtemp('dataset_u' + userId + '_t' + str(timestamp))
-    os.mkdir(tempPath + "/datafiles")
+    orig_path = os.getcwd()
+    with temporary_directory(export_file_root) as temp_path:
+      os.mkdir(temp_path + "/" + DATAFILES)
 
-    path, name = os.path.split(datasetFile)
-    size = os.stat(datasetFile).st_size
+      # Get the size of the gene list
+      path, name = os.path.split(dataset_file)
+      size = os.stat(dataset_file).st_size
 
-    # Copy the gene list dataset file under the datafiles folder of the temporary dir and change the filename to genelist.txt
-    shutil.copy(datasetFile, tempPath + "/datafiles/genelist.txt")
-    with open(tempPath + "/meta.json", "w+") as metaFile:
-        dump({'name': datasetName,
+      # Copy the gene list dataset file under the datafiles folder of the temporary dir and change the filename to genelist.txt since
+      # the WDK expects that file name for this type of data.
+      shutil.copy(dataset_file, temp_path + "/" + DATAFILES + "/" + GENE_LIST_FILE)
+
+      # Create and populate the meta.json file that must be included in the tarball
+      with open(temp_path + "/" + META_JSON, "w+") as metaFile:
+        dump({'name': dataset_name,
               'description': description,
               'summary': summary}, metaFile, indent=4)
 
-    # Create and populate the dataset.json file that must be included in the tarball
-    with open(tempPath + "/dataset.json", "w+") as datasetFile:
+      # Create and populate the dataset.json file that must be included in the tarball
+      # Don't know what the proper dependencies (if any) should be here
+      with open(temp_path + "/" + DATASET_JSON, "w+") as datasetFile:
         dump({
-          "type": {"name": "GeneList", "version": "1.0"},
+          "type": {"name": GENE_LIST_TYPE, "version": "1.0"},
            "dependencies":
              [{"resourceIdentifier": "pf3d7_genome_rsrc",
                "resourceVersion": "12/2/2015",
@@ -59,48 +91,61 @@ def __main__():
                "resourceVersion": "2.6",
                "resourceDisplayName": "human genome"}
               ],
-           "projects": ["PlasmoDB"],
-           "owner": userId,
+           "projects": [project],
+           "owner": user_id,
            "size": size,
            "modified": timestamp,
            "created": timestamp
         }, datasetFile, indent=4)
 
-    # The result file contains the information to be returned to the Galaxy user
-    with open(results, "w+") as resultsFile:
+      # The result file contains the information to be returned to the Galaxy user
+      with open(results, "w+") as resultsFile:
 
-        os.chdir(tempPath)
+        # Work inside the temporary directory.
+        os.chdir(temp_path)
 
-        fileRoot = "dataset_u" + userId + "_t" + str(timestamp)
-
-        # Package the tarball using the dataset_uNNNNN_tNNNNN.tgz convention
-        with tarfile.open(fileRoot + ".tgz", "w:gz") as tarball:
-            for item in ["meta.json", "dataset.json", "datafiles"]:
+        # Package the tarball - contains meta.json, dataset.json and a datafiles folder containing the gene list file
+        with tarfile.open(export_file_root + ".tgz", "w:gz") as tarball:
+            for item in [META_JSON, DATASET_JSON, DATAFILES]:
                 tarball.add(item)
 
         # Call the IRODS rest service to drop the tarball into the IRODS workspace landing zone
-        restUrl = "http://wij.vm:8180/irods-rest/rest/fileContents/ebrc/workspaces/lz/" + fileRoot + ".tgz"
-        headers = {"Accept":"application/json"}
-        file = {"uploadFile":open(fileRoot + ".tgz","rb")}
-        # TODO - POC only!! gotta salt away the usr/pwd somehow
-        auth = HTTPBasicAuth('wrkspuser', 'passWORD')
+        dataset_response = send_request(REST_URL, export_file_root + ".tgz", USER, PWD)
+        resultsFile.write("Tarball - " + str(dataset_response.status_code) + "\n" + dataset_response.text + "\n")
+        if dataset_response.status_code != requests.codes.ok:
+          sys.exit(os.EX_IOERR)
 
-        response = requests.post(restUrl, auth=auth, headers=headers, files=file)
-        resultsFile.write("Tarball - " + str(response.status_code) + "\n" + response.text + "\n")
+        # Create a empty (flag) file corresponding to the tarball
+        open(export_file_root + ".txt", "w").close()
 
         # Call the IRODS rest service to drop a flag into the IRODS workspace landing zone.  This flag
         # triggers the IRODS PEP that unpacks the tarball and posts the event to Jenkins
-        with open(fileRoot + ".txt", "w+") as flag:
-            flag.write("flag")
-        restUrl = "http://wij.vm:8180/irods-rest/rest/fileContents/ebrc/workspaces/lz/" + fileRoot + ".txt"
-        file = {"uploadFile": open(fileRoot + ".txt", "rb")}
-        response = requests.post(restUrl, auth=auth, headers=headers, files=file)
-        resultsFile.write("Flag - " + str(response.status_code) + "\n" + response.text + "\n")
+        flag_response = send_request(REST_URL, export_file_root + ".txt", USER, PWD)
+        resultsFile.write("Flag - " + str(flag_response.status_code) + "\n" + flag_response.text + "\n")
+        if flag_response.status_code != requests.codes.ok:
+          sys.exit(os.EX_IOERR)
 
-        # TODO - throw away the temp dir.
-        resultsFile.write("The temporary dataset directory is " + tempPath)
+      # Step out of the temp directory
+      os.chdir(orig_path)
 
 
+# This request is intended as a multi-part form post containing one file to be uploaded.  IRODS Rest does an iput
+# followed by an iget, apparently.  So the response can be used to insure proper delivery.
+def send_request(url, export_file, user, password):
+  request = url + export_file
+  headers = {"Accept": "application/json"}
+  file = {"uploadFile": open(export_file, "rb")}
+  auth = HTTPBasicAuth(user, password)
+  response = requests.post(request, auth=auth, headers=headers, files=file)
+  return response
 
+# Creates a temporary directory such that removal is assured at end of program
+@contextlib.contextmanager
+def temporary_directory(dir_name):
+  temp_path = tempfile.mkdtemp(dir_name)
+  try:
+    yield temp_path
+  finally:
+    shutil.rmtree(temp_path)
 
 if __name__ == "__main__": __main__()
