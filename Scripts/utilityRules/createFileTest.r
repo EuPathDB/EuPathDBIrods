961685a88cd742d0263b951d0eff0b1fe95aea4e
Cris Lawrence <crisl@upenn.edu>
2017-01-27 22:16:40 +0000

    Create irods file test.
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/EuPathDBIrods/trunk@79458 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Scripts/utilityRules/createFileTest.r
+++ Scripts/utilityRules/createFileTest.r
@@ -1,8 +1,9 @@
 utilCreateAFile {
   writeLine("stdout", "Starting creation of a file");
   msiDataObjCreate(*dataObj,"forceFlag=",*fileDescriptor);
+  msiDataObjWrite(*fileDescriptor,"I am in!",*fileSize);
   msiDataObjClose(*fileDescriptor,*status);
   writeLine("stdout", "File creation complete");
 }
-input  *dataObj = "/ebrc/workspaces/A"
+input  *dataObj = "/ebrc/workspaces/flags/success-test"
 output ruleExecOut
