4386bda59640aceab9af6d41529fab8c9a641f20
Cris Lawrence <crisl@upenn.edu>
2017-04-05 19:27:48 +0000

    Improvements to documentation
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/EuPathDBIrods/trunk@80791 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Controller/src/main/java/org/apidb/irods/BuildEventsFile.java
+++ Controller/src/main/java/org/apidb/irods/BuildEventsFile.java
@@ -16,7 +16,6 @@ import org.gusdb.wdk.model.config.ModelConfigUserDatasetStore;
 import org.gusdb.wdk.model.user.dataset.UserDatasetStore;
 import org.gusdb.wdk.model.user.dataset.UserDatasetStoreAdaptor;
 import org.gusdb.wdk.model.user.dataset.event.UserDatasetEventArrayHandler;
-import org.gusdb.wdk.model.user.dataset.event.UserDatasetEventListHandler;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.xml.sax.SAXException;
@@ -24,9 +23,9 @@ import org.xml.sax.SAXException;
 /**
  * This is a small Java application that should be called by Jenkins whenever an IRODS event of note takes
  * place and also periodically, given the possibility that a prior event was not captured.  It reads the
- * contents of each file in the entire IRODS events folder, and consolidates them into a single list of events
- * that is passed on to the WDK's user dataset events handler to processing according to the nature of each
- * event. Although this file addresses event handling is intended for IRODS, it has no specific references
+ * contents of each file in the entire IRODS events folder, and consolidates them into a single JSON array of events
+ * that is passed on to the WDK's user dataset events handler for processing according to the nature of each
+ * event. Although this project is specifically meant to address event handling intended for IRODS, it has no specific references
  * to IRODS.  It could be used with a POSIX system as well.
  * 
  * @author crisl-adm
@@ -37,10 +36,18 @@ public class BuildEventsFile {
   public static final String EVENTS_DIR = "/ebrc/workspaces/events";
 
   public static void main(String[] args) throws Exception {
+	  
+	// The id of the project for which these events are intended.
     String projectId = System.getenv("PROJECT_ID");
+    
+    // Identifies the datastore associated with the events to be handled.  Used to insure that this datastore
+    // matches the one this build supports.
     String datasetStoreId = System.getenv("DATASET_STORE_ID");
+ 
     logger.info("Parameters - Project: " + projectId + ", Dataset Store: " + datasetStoreId);
-    String gusHome = System.getProperty(Utilities.SYSTEM_PROPERTY_GUS_HOME);     
+    
+    // A model configuration file must exist for every project this program may possibly support.
+    String gusHome = System.getProperty(Utilities.SYSTEM_PROPERTY_GUS_HOME);
     ModelConfigParser parser = new ModelConfigParser(gusHome);
     ModelConfig modelConfig = null;
     try {
@@ -50,12 +57,15 @@ public class BuildEventsFile {
       e.printStackTrace();
       throw new RuntimeException(e);
     }
+    
+    // Insures that the appropriate datastore is calling this method.
     ModelConfigUserDatasetStore udsConfig = modelConfig.getUserDatasetStoreConfig();
     UserDatasetStore uds = udsConfig.getUserDatasetStore();
-    // Insures that only the correct IRODS is calling this method.
     if(!uds.getUserDatasetStoreId().equals(datasetStoreId)) {
-      throw new RuntimeException("Called by wrong IRODS " + datasetStoreId);
+      throw new RuntimeException("Called by wrong datastore " + datasetStoreId);
     }
+    
+    // Collect all the event files from the events folder in the datastore.
     UserDatasetStoreAdaptor udsa = uds.getUserDatasetStoreAdaptor();
     List<Path> eventFiles = new ArrayList<>();
     try {
@@ -65,20 +75,25 @@ public class BuildEventsFile {
       e.printStackTrace();
       throw new RuntimeException(e);
     }
+    
+    // Read the contents of those json formatted event files into JSON objects and collect
+    // those JSON objects into a JSON array.
     JSONArray eventJsonArray = new JSONArray();
     for(Path eventFile : eventFiles) {
-      if(eventFile.getFileName().toString().endsWith(".json")) {	
+      if(eventFile.getFileName().toString().endsWith(".json")) {
         String event = udsa.readFileContents(eventFile);
         JSONObject eventJson = new JSONObject(event);
         eventJsonArray.put(eventJson);
       }  
     }
-    System.out.println(eventJsonArray.toString());
+    logger.info("Events JSON array:" + eventJsonArray.toString());
+    
+    // Create a dataset event handler to further process the resulting events JSON array.
     UserDatasetEventArrayHandler handler = new UserDatasetEventArrayHandler();
     handler.setProjectId(projectId);
     Path tmpDir =  Paths.get(handler.getWdkTempDirName());
     handler.handleEventList(UserDatasetEventArrayHandler.parseEventsArray(eventJsonArray),
-        modelConfig.getUserDatasetStoreConfig().getTypeHandlers(), tmpDir);
+                            modelConfig.getUserDatasetStoreConfig().getTypeHandlers(), tmpDir);
   }
 
 }
