fd1d902848bd11604f5a625feb4551fa6cfae76a
Elizabeth Harper <foxcapade@gmail.com>
2019-09-05 21:02:37 +0000

    Correct potential NPE in BuildEventsFile
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/EuPathDBIrods/trunk@100844 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Controller/src/main/java/org/apidb/irods/BuildEventsFile.java
+++ Controller/src/main/java/org/apidb/irods/BuildEventsFile.java
@@ -22,27 +22,27 @@ import org.json.JSONObject;
  * that is passed on to the WDK's user dataset events handler for processing according to the nature of each
  * event. Although this project is specifically meant to address event handling intended for IRODS, it has no specific references
  * to IRODS.  It could be used with a POSIX system as well.
- * 
+ *
  * @author crisl-adm
  */
 public class BuildEventsFile {
 
-  private static final Logger logger = Logger.getLogger(BuildEventsFile.class);	
+  private static final Logger logger = Logger.getLogger(BuildEventsFile.class);
   public static final String EVENTS_DIR = "/ebrc/workspaces/events";
 
   public static void main(String[] args) throws Exception {
-	  
+
 	// The id of the project for which these events are intended.
     String projectId = System.getenv("PROJECT_ID");
-    
+
     // Identifies the datastore associated with the events to be handled.  Used to insure that this datastore
     // matches the one this build supports.
     String datasetStoreId = System.getenv("DATASET_STORE_ID");
- 
+
     logger.info("Parameters - Project: " + projectId + ", Dataset Store: " + datasetStoreId);
-    
+
     JSONArray eventJsonArray = new JSONArray();
-    
+
     // Create a dataset event handler to process the resulting events JSON array.  The
     // dataset event handler constructor uses the provided projectId to initialize and
     // populate the user dataset store and provide the temporary directory url.
@@ -56,10 +56,10 @@ public class BuildEventsFile {
       e.printStackTrace();
       throw new RuntimeException(e);
     }
-     
+
     // Open the user dataset session for processing the event list.
     try(UserDatasetSession dsSession = dsStore.getSession(dsStore.getUsersRootDir())) {
-    	
+
       // Insure that the dataset store that triggered this event handling operation is the
       // same as the one with which this code communicates.
       if(dsStore.getId() == null || !dsStore.getId().equals(datasetStoreId)) {
@@ -76,11 +76,11 @@ public class BuildEventsFile {
       catch (Exception e) {
         throw new WdkModelException(e);
       }
-      
+
       // Read the contents of recent json formatted event files into JSON objects and collect
       // those JSON objects into a JSON array.
-      //TODO - eventJsonArray could produce big memory footprint if we handle large number of event files. 
-      for(Path eventFile : dsSession.getRecentEvents(EVENTS_DIR, lastHandledEventId)) {
+      //TODO - eventJsonArray could produce big memory footprint if we handle large number of event files.
+      for(Path eventFile : dsSession.getRecentEvents(EVENTS_DIR, lastHandledEventId == null ? 0L : lastHandledEventId)) {
         if(eventFile.getFileName().toString().endsWith(".json")) {
           String event = dsAdaptor.readFileContents(eventFile);
           JSONObject eventJson = new JSONObject(event);
