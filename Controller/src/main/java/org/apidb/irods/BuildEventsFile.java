abd27176b27df2f04c2046b50d7cbe45657aa2b9
Elizabeth Harper <epharper@upenn.edu>
2021-06-09 16:22:39 -0400

    Cleanup diff.

--- Controller/src/main/java/org/apidb/irods/BuildEventsFile.java
+++ Controller/src/main/java/org/apidb/irods/BuildEventsFile.java
@@ -1,12 +1,17 @@
 package org.apidb.irods;
 
+import java.nio.file.Path;
 import java.util.LinkedList;
+
+import javax.sql.DataSource;
 
 import org.apache.log4j.Logger;
 import org.gusdb.fgputil.db.pool.DatabaseInstance;
 import org.gusdb.fgputil.json.JsonUtil;
 import org.gusdb.wdk.model.WdkModel;
 import org.gusdb.wdk.model.WdkModelException;
+import org.gusdb.wdk.model.user.dataset.UserDatasetSession;
+import org.gusdb.wdk.model.user.dataset.UserDatasetStoreAdaptor;
 import org.gusdb.wdk.model.user.dataset.event.UserDatasetEventCleanup;
 import org.gusdb.wdk.model.user.dataset.event.UserDatasetEventSync;
 import org.gusdb.wdk.model.user.dataset.event.raw.EventParser;
@@ -19,7 +24,7 @@ import org.gusdb.wdk.model.user.dataset.event.raw.UDEvent;
  * the entire iRODS events folder, and consolidates them into a single JSON
  * array of events that is passed on to the WDK's user dataset events handler
  * for processing according to the nature of each event.
- * <p>
+ *
  * Although this project is specifically meant to address event handling
  * intended for iRODS, it has no specific references to iRODS.  It could be used
  * with a POSIX system as well.
@@ -28,21 +33,21 @@ import org.gusdb.wdk.model.user.dataset.event.raw.UDEvent;
  */
 public class BuildEventsFile {
 
-  private static final Logger LOG        = Logger.getLogger(BuildEventsFile.class);
-  public static final  String EVENTS_DIR = "/ebrc/workspaces/events";
+  private static final Logger logger = Logger.getLogger(BuildEventsFile.class);
+  public static final String EVENTS_DIR = "/ebrc/workspaces/events";
 
   public static void main(String[] args) throws Exception {
 
-    // The id of the project for which these events are intended.
-    var projectId = System.getenv("PROJECT_ID");
+	// The id of the project for which these events are intended.
+    String projectId = System.getenv("PROJECT_ID");
 
     // Identifies the datastore associated with the events to be handled.
     // Used to insure that this datastore matches the one this build supports.
-    var datasetStoreId = System.getenv("DATASET_STORE_ID");
+    String datasetStoreId = System.getenv("DATASET_STORE_ID");
 
-    var mode = System.getenv("RUN_MODE");
+    String mode = System.getenv("RUN_MODE");
 
-    LOG.info("Parameters - Project: " + projectId
+    logger.info("Parameters - Project: " + projectId
       + ", Dataset Store: " + datasetStoreId
       + ", Run Mode: " + mode);
 
@@ -54,20 +59,22 @@ public class BuildEventsFile {
         cleanupMode(projectId);
         break;
       default:
-        LOG.error("Unset or invalid RUN_MODE value.  Must be one of \"sync\" or \"cleanup\".");
+        logger.error("Unset or invalid RUN_MODE value.  Must be one of \"sync\" or \"cleanup\".");
     }
   }
 
   private static void syncMode(String projectId, String datasetStoreId) throws WdkModelException {
-    // Create a dataset event handler to process the resulting events JSON
-    // array.  The dataset event handler constructor uses the provided projectId
+    var eventJsonArray = new LinkedList<UDEvent>();
+
+    // Create a dataset event handler to process the resulting events list.
+    // The dataset event handler constructor uses the provided projectId
     // to initialize and populate the user dataset store and provide the
     // temporary directory url.
     var handler = new UserDatasetEventSync(projectId);
     var dsStore = handler.getUserDatasetStore();
 
     // Open the user dataset session for processing the event list.
-    try (var dsSession = dsStore.getSession(dsStore.getUsersRootDir())) {
+    try(UserDatasetSession dsSession = dsStore.getSession(dsStore.getUsersRootDir())) {
 
       // Insure that the dataset store that triggered this event handling
       // operation is the same as the one with which this code communicates.
@@ -77,39 +84,30 @@ public class BuildEventsFile {
 
       // Collect a subset of the event files from the events folder in the
       // datastore.
-      var  dsAdaptor = dsSession.getUserDatasetStoreAdaptor();
-      Long lastHandledEventId;
-      try (
-        DatabaseInstance appDb = new DatabaseInstance(
-          handler.getModelConfig().getAppDB(),
-          WdkModel.DB_INSTANCE_APP,
-          true
-        )
-      ) {
-        lastHandledEventId = handler.findLastHandledEvent(appDb.getDataSource());
+      UserDatasetStoreAdaptor dsAdaptor = dsSession.getUserDatasetStoreAdaptor();
+      Long lastHandledEventId = null;
+      try (DatabaseInstance appDb = new DatabaseInstance(handler.getModelConfig().getAppDB(), WdkModel.DB_INSTANCE_APP, true)) {
+          DataSource appDbDataSource = appDb.getDataSource();
+          lastHandledEventId = handler.findLastHandledEvent(appDbDataSource);
+      }
+      catch (Exception e) {
+        throw new WdkModelException(e);
       }
 
       // Read the contents of recent json formatted event files into JSON
       // objects and collect those JSON objects into a JSON array.
-      // TODO - eventJsonArray could produce big memory footprint if we handle large number of event files.
-      var eventJsonArray = new LinkedList<UDEvent>();
-      var eventFiles = dsSession.getRecentEvents(
-        EVENTS_DIR,
-        lastHandledEventId == null ? 0L : lastHandledEventId
-      );
-
-      for (var eventFile : eventFiles) {
-        if (eventFile.getFileName().toString().endsWith(".json")) {
-          eventJsonArray.add(EventParser.parseSingle(dsAdaptor.readFileContents(eventFile)));
+      //TODO - eventJsonArray could produce big memory footprint if we handle large number of event files.
+      for(Path eventFile : dsSession.getRecentEvents(EVENTS_DIR, lastHandledEventId == null ? 0L : lastHandledEventId)) {
+        if(eventFile.getFileName().toString().endsWith(".json")) {
+          String event = dsAdaptor.readFileContents(eventFile);
+          var eventJson = EventParser.parseSingle(event);
+          eventJsonArray.add(eventJson);
         }
       }
-
-      LOG.info("Events JSON array:" + JsonUtil.prettyPrint(eventJsonArray));
+      logger.info("Events JSON array:" + JsonUtil.prettyPrint(eventJsonArray));
 
       // Process the events
       handler.handleEventList(UserDatasetEventSync.parseEventsArray(eventJsonArray));
-    } catch (Exception e) {
-      throw new WdkModelException(e);
     }
   }
 
