be94f7d558163309009c5db79d1a809cae26c4db
Elizabeth <foxcapade@gmail.com>
2022-06-03 10:42:07 -0400

    add new env arg for max events per sync

--- Controller/src/main/java/org/apidb/irods/ProcessIrodsEvents.java
+++ Controller/src/main/java/org/apidb/irods/ProcessIrodsEvents.java
@@ -29,23 +29,72 @@ import org.gusdb.wdk.model.user.dataset.event.raw.UDEvent;
  * intended for iRODS, it has no specific references to iRODS.  It could be used
  * with a POSIX system as well.
  *
+ * <h2>Runtime Configuration</h2>
+ *
+ * This script requires the following configuration to be set on the environment
+ * to execute:
+ *
+ * <dl>
+ *   <dt><code>PROJECT_ID</code></dt>
+ *   <dd><b>REQUIRED</b> Target project that events should be processed for.
+ *       Events that are for other projects will be skipped.</dd>
+ *   <dt><code>DATASET_STORE_ID</code></dt>
+ *   <dd><b>REQUIRED</b> Identifies the datastore associated with the events to
+ *       be handled.  This value is used to ensure that this datastore matches
+ *       the one the current build supports.</dd>
+ *   <dt><code>RUN_MODE</code></dt>
+ *   <dd><b>REQUIRED</b> Selects the operation mode the script will execute in.
+ *       Must be one of &quot;<code>sync</code>&quot; or
+ *       &quot;<code>cleanup</code>&quot;.  <code>sync</code> mode synchronizes
+ *       the datasets from iRODS into the database.  <code>cleanup</code> mode
+ *       performs cleanup steps to resolve issues encountered when last syncing
+ *       datasets.</dd>
+ *   <dt><code>MAX_EVENTS</code></dt>
+ *   <dd><b>OPTIONAL</b> Sets the maximum number of events to process in a
+ *       single execution of the script.  This value paginates the events coming
+ *       from iRODS to avoid issues with connection leaks that are encountered
+ *       on long running Jargon-iRODS sessions.<br><br>Defaults to 150</dd>
+ * </dl>
+ *
  * @author crisl-adm
  */
 public class ProcessIrodsEvents {
 
+  // region Environment configuration variables
+
+  private static final String ENV_PROJECT_ID = "PROJECT_ID";
+
+  private static final String ENV_DATASTORE_ID = "DATASET_STORE_ID";
+
+  private static final String ENV_RUN_MODE = "RUN_MODE";
+
+  private static final String ENV_MAX_EVENTS = "MAX_EVENTS";
+
+  // endregion Environment configuration variables
+
+
   private static final Logger logger = Logger.getLogger(ProcessIrodsEvents.class);
+
+  /**
+   * Default maximum number of events to handle per script execution.
+   *
+   * This value may be overridden using the environment variable
+   * {@code MAX_EVENTS}.
+   */
+  private static final int DEFAULT_MAX_EVENTS_PER_EXEC = 150;
+
   public static final String EVENTS_DIR = "/ebrc/workspaces/events";
 
   public static void main(String[] args) throws Exception {
 
-	// The id of the project for which these events are intended.
-    String projectId = System.getenv("PROJECT_ID");
+	  // The id of the project for which these events are intended.
+    String projectId = System.getenv(ENV_PROJECT_ID);
 
     // Identifies the datastore associated with the events to be handled.
-    // Used to insure that this datastore matches the one this build supports.
-    String datasetStoreId = System.getenv("DATASET_STORE_ID");
+    // Used to ensure that this datastore matches the one this build supports.
+    String datasetStoreId = System.getenv(ENV_DATASTORE_ID);
 
-    String mode = System.getenv("RUN_MODE");
+    String mode = System.getenv(ENV_RUN_MODE);
 
     logger.info("Parameters - Project: " + projectId
       + ", Dataset Store: " + datasetStoreId
@@ -76,7 +125,7 @@ public class ProcessIrodsEvents {
     // Open the user dataset session for processing the event list.
     try(UserDatasetSession dsSession = dsStore.getSession(dsStore.getUsersRootDir())) {
 
-      // Insure that the dataset store that triggered this event handling
+      // Ensure that the dataset store that triggered this event handling
       // operation is the same as the one with which this code communicates.
       if(dsStore.getId() == null || !dsStore.getId().equals(datasetStoreId)) {
         throw new RuntimeException("Called by wrong datastore " + datasetStoreId + ". Expecting " + dsStore.getId());
@@ -109,11 +158,38 @@ public class ProcessIrodsEvents {
       logger.info("Events JSON array:" + JsonUtil.prettyPrint(eventJsonArray));
 
       // Process the events
-      handler.handleEventList(UserDatasetEventSync.parseEventsArray(eventJsonArray));
+      handler.handleEventList(
+        UserDatasetEventSync.parseEventsArray(eventJsonArray),
+        getMaxEventsPerExecution()
+      );
     }
   }
 
   private static void cleanupMode(String projectID) throws WdkModelException {
     new UserDatasetEventCleanup(projectID).cleanupFailedInstalls();
+  }
+
+  /**
+   * Parses the {@link ProcessIrodsEvents#ENV_MAX_EVENTS} environment variable
+   * into an int value, or defaults to
+   * {@link ProcessIrodsEvents#DEFAULT_MAX_EVENTS_PER_EXEC}.
+   *
+   * This value controls the max number of iRODS events that will be processed
+   * in a single execution of this script.
+   *
+   * @return Maximum number of events to handle in a single script execution.
+   */
+  private static int getMaxEventsPerExecution() {
+    String env = System.getenv(ENV_MAX_EVENTS);
+
+    if (env == null || env.isBlank())
+      return DEFAULT_MAX_EVENTS_PER_EXEC;
+
+    try {
+      return Integer.parseInt(env);
+    } catch (Throwable e) {
+      logger.warn("MAX_EVENTS was not a valid int value, defaulting to " + DEFAULT_MAX_EVENTS_PER_EXEC);
+      return DEFAULT_MAX_EVENTS_PER_EXEC;
+    }
   }
 }
